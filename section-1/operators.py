852d5ef26c91834c291e7041f03fec6b25926945
malindasuhash <malinda.suhash@gmail.com>
2023-02-28 22:36:19 +0000

    continuing

---
+++ section-1/operators.py
@@ -0,0 +1,20 @@
+print(10 / 3) # Prints 3.3333
+print(10 // 3) # Prints 3 truncates the remainder
+print(10.0 / 3.0) # Prints 3.33333
+print(10.0 // 3.0) # Prints 3.0 Take a note of the decimal place
+print(10 / 3.0 ) # Prints 3.33333
+print(10 // 3.0) # Prints 3.0; note the truncation but decimal place
+print(10.0 / 3) # Prints 3.33333
+print(10.0 // 3) # Prints 3.0; note the truncation and decimal place
+
+# // is an integer division operator
+
+def square(x):
+    return x * x
+
+def sub(x, y):
+    return x - y
+
+print(square(10))
+
+print(square)
