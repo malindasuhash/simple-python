f20e7450c04454c13df7b2647f0b15f2f8fed317
malindasuhash <malinda.suhash@gmail.com>
2023-03-07 23:35:17 +0000

    simple operator code

--- section-1/simple-calc.py
+++ section-1/simple-calc.py
@@ -1,9 +1,10 @@
 
 def hoursMinSecs(seconds):
     hours = seconds // 3600 # Number of seconds in an hour (integer division)
-    secsLeft = seconds % 3600 # Determines the seconds left out of hours (modulo)
-    mins = secsLeft // 60 # Out of the seconds; determines mins
-    secsLeft = secsLeft % 60 # Seconds left
-    print("Hours=" + str(hours) + " Mins=" + str(mins) + " Seconds=" + str(secsLeft))
+    secs_still_remaining = seconds % 3600 # Determines the seconds left out of hours (modulo)
+    minutes = secs_still_remaining // 60 # Out of the seconds; determines mins
+    seconds_finally_remaining = secs_still_remaining % 60 # Seconds left
+    print("Hours=" + str(hours) + " Mins=" + str(minutes) + " Seconds=" + str(seconds_finally_remaining))
 
+# Code updated to have better named variables
 print(hoursMinSecs(3905))
